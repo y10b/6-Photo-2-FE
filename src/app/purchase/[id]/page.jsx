692418cba628040db7e313dd957da868a0fad4e4
y10b <victory.jun01@gmail.com>
2025-05-19 19:58:19 +0900

    repact: api 분리 리팩토링

--- src/app/purchase/[id]/page.jsx
+++ src/app/purchase/[id]/page.jsx
@@ -5,18 +5,7 @@ import { useParams } from "next/navigation";
 import NoHeader from "@/components/layout/NoHeader";
 import CardProfile from "@/components/ui/card/cardProfile/CardProfile";
 import Image from "next/image";
-
-async function fetchPurchase(id) {
-  const res = await fetch(`http://localhost:5005/api/purchase/${id}`, {
-    next: { revalidate: 0 },
-  });
-
-  if (!res.ok) throw new Error("포토카드 정보를 불러올 수 없습니다");
-
-  const result = await res.json();
-  return result.data;
-}
-
+import { fetchPurchase } from "@/lib/api/purchase";
 function PurchasePage() {
   const { id } = useParams();
   const [photoCard, setPhotoCard] = useState(null);
