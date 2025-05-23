3e225f6a275b8367044755cf8016a147dd33f374
jbinyim <jbinyim12@naver.com>
2025-05-23 14:20:41 +0900

    fix: 버튼 컴포넌트 사용

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
-import Button from "@/components/common/Button";
-import Image from "next/image";
-import Link from "next/link";
-
+import Button from '@/components/common/Button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
       <div className="max-w-[744px] mx-auto overflow-x-hidden tablet:max-w-[1200px] pc:max-w-full">
         <section className="bg-custom-landing1bg max-w-[1798px] mx-auto h-[412px] rounded-[24px] flex flex-col items-center tablet:h-[722px] pc:h-[1088px]">
           <Image
-            src={"/logo.svg"}
+            src={'/logo.svg'}
             width={138}
             height={25}
             alt="로고"
@@ -24,17 +23,15 @@ export default function Home() {
             </p>
           </div>
           <Link
-            href={"/product"}
-            className="text-black font-bold text-[12px] mt-[24px] mb-[26px] tablet:mt-[38px] tablet:mb-[37px] tablet:text-[16px] pc:mt-[33px] pc:mb-[0]"
+            href="/product"
+            className="mt-[24px] mb-[26px] tablet:mt-[38px] tablet:mb-[37px] pc:mt-[33px] pc:mb-[0]"
           >
-            <p className="w-[150px] h-[40px] flex items-center justify-center bg-main tablet:w-[226px] tablet:h-[55px]">
-              최애 찾으러 가기
-            </p>
+            <Button role="navigate">최애 찾으러 가기</Button>
           </Link>
           <figure className="relative w-[calc(100%+120px)] h-[199px] overflow-visible tablet:h-[352px] pc:h-[765px]">
             <Image
               className="absolute object-contain"
-              src={"/images/landing/landing1.svg"}
+              src={'/images/landing/landing1.svg'}
               fill
               alt="랜딩 페이지 최애 포토 바로 가기"
             />
@@ -48,7 +45,7 @@ export default function Home() {
       <section className="mt-[60px] mb-[116px] flex flex-col items-center justify-center gap-[23px] tablet:gap-[27px] tablet:mb-[190px] pc:mt-[114px] pc:mb-[161px]">
         <figure className="relative w-[95px] h-[114px] tablet:w-[130px] tablet:h-[150px]">
           <Image
-            src={"/images/landing/landing5.svg"}
+            src={'/images/landing/landing5.svg'}
             fill
             alt="landing5"
             className="object-cover"
@@ -58,12 +55,10 @@ export default function Home() {
           나의 최애를 지금 찾아보세요!
         </p>
         <Link
-          href={"/product"}
-          className="text-black font-bold text-[12px] mt-[24px] mb-[26px] tablet:mt-[38px] tablet:mb-[37px] tablet:text-[16px] pc:mt-[33px] pc:mb-[0]"
+          href="/product"
+          className="mt-[24px] mb-[26px] tablet:mt-[38px] tablet:mb-[37px] pc:mt-[33px] pc:mb-[0]"
         >
-          <p className="w-[150px] h-[40px] flex items-center justify-center bg-main tablet:w-[226px] tablet:h-[55px]">
-            최애 찾으러 가기
-          </p>
+          <Button role="navigate">최애 찾으러 가기</Button>
         </Link>
       </section>
     </div>
