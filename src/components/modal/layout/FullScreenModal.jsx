2f8ea35e95ae97be9a5deda346d96a4c5c8e4058
elisaohh <elisaohh@gmail.com>
2025-05-27 09:25:12 +0900

    feat: 풀스크린 모달 레이아웃 컴포넌트 구현

--- src/components/modal/layout/FullScreenModal.jsx
+++ src/components/modal/layout/FullScreenModal.jsx
@@ -1,7 +1,22 @@
 'use client';
 
-export default function FullScreenModal({children, onClose}) {
-  return;
-}
+import Image from 'next/image';
 
-// 얼른 구현 마치겠습니다 ~~~ ㅠㅠ
+export default function FullScreenModal({children, onClose}) {
+  return (
+    <div className="fixed inset-0 z-50 bg-black flex justify-center items-center">
+      <div className="w-full h-full bg-black text-white relative overflow-auto">
+        {/* 상단 뒤로가기 버튼 */}
+        <button
+          onClick={onClose}
+          className="absolute top-4 left-4 z-10 text-white"
+        >
+          <Image src="/icons/ic_back.svg" width={24} height={24} />
+        </button>
+
+        {/* 콘텐츠 */}
+        <div className="p-4 pt-12">{children}</div>
+      </div>
+    </div>
+  );
+}
