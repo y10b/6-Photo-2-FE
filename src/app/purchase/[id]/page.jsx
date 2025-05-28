2779616fc3cc4debc0b6b5f7d65383d6c15d5e2a
y10b <victory.jun01@gmail.com>
2025-05-28 17:29:32 +0900

    refactor: 구매 페이지 스켈레톤 UI 추가

--- src/app/purchase/[id]/page.jsx
+++ src/app/purchase/[id]/page.jsx
@@ -6,6 +6,8 @@ import ExchangeInfoSection from '@/components/exchange/ExchangeInfoSection';
 import {fetchPurchase} from '@/lib/api/purchase';
 import {fetchMyCards} from '@/lib/api/shop';
 import CardDetailSection from '@/components/common/TransactionSection';
+import TransactionSection from '@/components/ui/skeleton/TransactionSkeleton';
+import ExchangeInfoSkeleton from '@/components/ui/skeleton/ExchangeInfoSkeleton';
 
 export default function PurchasePage() {
   const {id} = useParams();
@@ -13,13 +15,26 @@ export default function PurchasePage() {
   const [photoCard, setPhotoCard] = useState(null);
   const [myCards, setMyCards] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [showSkeleton, setShowSkeleton] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    let timeoutId;
+
     if (id) {
       setIsLoading(true);
-      loadData(id);
+      setShowSkeleton(false);
+
+      timeoutId = setTimeout(() => {
+        setShowSkeleton(true); // 3초 후 skeleton 표시
+      }, 3000);
+
+      loadData(id).then(() => {
+        setIsLoading(false); // 로딩 끝
+      });
     }
+
+    return () => clearTimeout(timeoutId); // cleanup
   }, [id]);
 
   const loadData = async shopId => {
@@ -48,12 +63,16 @@ export default function PurchasePage() {
     }
   };
 
-  /* 이후 스켈레톤 UI 고려 */
-  if (isLoading) {
-    return <div>로딩중...</div>;
+  if (isLoading && showSkeleton) {
+    return (
+      <div className="mx-auto w-[345px] tablet:w-[704px] pc:w-[1480px]">
+        <TransactionSection type="buyer" />
+        <ExchangeInfoSkeleton />
+      </div>
+    );
   }
 
-  if (!photoCard) {
+  if (!photoCard && isLoading) {
     return (
       <div className="text-white text-center mt-10">
         포토카드를 찾을 수 없습니다.
