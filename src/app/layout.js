4a36f2d377b0176b314f0cdc66aff2769c30f60a
y10b <victory.jun01@gmail.com>
2025-05-31 18:51:37 +0900

    style: 교환 카드 닉네임 부분 스타일링 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ import ModalRenderer from '@/components/modal/ModalRenderer';
 export const metadata = {
   title: '최애의 포토',
   description: '디지털 포토카드를 사고팔고 교환할 수 있는 팬 컬렉터블 플랫폼. K-POP, 스포츠 등 다양한 분야의 포토카드를 거래하며 나만의 디지털 앨범을 완성해보세요. 실시간 거래, 포인트 시스템, 교환 기능, 알림 시스템 등 다양한 기능을 통해 새로운 수집 문화를 경험할 수 있습니다.',
+  icons: {
+    icon: "/icons/ic_pavicon.svg"
+  }
 };
 
 export default function RootLayout({ children }) {
