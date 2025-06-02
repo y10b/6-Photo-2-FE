90ca0bea638869717e9555e4abf0db6501449f37
y10b <victory.jun01@gmail.com>
2025-06-02 13:35:03 +0900

    fix: isSeller 플래그를 활용하여 구매자인지 판매자인지 구분

--- src/lib/api/purchase.js
+++ src/lib/api/purchase.js
@@ -1,6 +1,7 @@
-const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
-// 판매 정보 가져오기
+/* const BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`; */
+const BASE_URL = 'http://localhost:5005/api';
 
+// 판매 정보 가져오기
 export async function fetchPurchase(shopId, accessToken) {
   const res = await fetch(`${BASE_URL}/purchase/${shopId}`, {
     method: 'GET',
@@ -20,11 +21,18 @@ export async function fetchPurchase(shopId, accessToken) {
 
   if (res.ok) {
     const result = await res.json();
-    return result.data;
+
+    // 판매자인 경우 메시지에 '본인이 등록한 판매 게시글입니다'라는 문구가 포함됨
+    const isSeller = result.message?.includes('본인이 등록한 판매 게시글') ||
+      result.data?.isSeller === true;
+
+    return {
+      ...result.data,
+      isSeller,
+    };
   }
 
   throw new Error("포토카드 정보를 불러올 수 없습니다");
-
 }
 
 // 카드 구매 요청 보내기
