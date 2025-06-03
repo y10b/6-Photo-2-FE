2c9d26bf18ba974df733750e1d12ffd23e0cc0e1
elisaohh <elisaohh@gmail.com>
2025-06-03 11:21:40 +0900

    refactor: 커스텀 훅으로 리팩토링

---
+++ src/utils/filterOptions.js
@@ -0,0 +1,30 @@
+export const GRADE_OPTIONS = [
+  {label: 'COMMON', value: 'COMMON'},
+  {label: 'RARE', value: 'RARE'},
+  {label: 'SUPER RARE', value: 'SUPER_RARE'},
+  {label: 'LEGENDARY', value: 'LEGENDARY'},
+];
+
+export const GENRE_OPTIONS = [
+  {label: '여행', value: 'TRAVEL'},
+  {label: '풍경', value: 'LANDSCAPE'},
+  {label: '인물', value: 'PORTRAIT'},
+  {label: '사물', value: 'OBJECT'},
+];
+
+export const SOLDOUT_OPTIONS = [
+  {label: '판매 중', value: 'false'},
+  {label: '품절', value: 'true'},
+];
+
+export const METHOD_OPTIONS = [
+  {label: '판매', value: 'sale'},
+  {label: '교환', value: 'exchange'},
+];
+
+export const SORT_OPTIONS = [
+  {label: '최신순', value: 'latest'},
+  {label: '오래된순', value: 'oldest'},
+  {label: '낮은 가격순', value: 'price-asc'},
+  {label: '높은 가격순', value: 'price-desc'},
+];
